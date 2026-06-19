5022daf6604219c8b0c6d7b8c7c19d91c6147a0f
yslee <leeyeso9072@gmail.com>
2026-06-19 12:43:42 +0900

    feat: Main.java 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * 설명 : Main
+ * 
+ * @author yslee
+ * @since 2026. 6. 19.
+ */
+public class Main {
+
+	// 2026. 6. 19. yslee - 어떤 코드 수정사항 적용 내용
+	// 기존 코드: 기존 코드 설명
+	// 문제: 기존 코드 문제 설명
+	// 개선: 개선사항 내용
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
